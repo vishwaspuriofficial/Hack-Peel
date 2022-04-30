ab880aba58f3efea6561c0206c21745eff101be9
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-04-29 22:38:07 -0400

    Added loadDates() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
+import java.io.File;
+import java.util.LinkedList;
+import java.io.*;
+
 public class Main {
+
+    public static void main(String[] agrs)  {
+    }
+    LinkedList<String> dates;
+    String path = "../Databases/mainsave.txt";
+    public LinkedList<String> loadDates() throws IOException {
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        int r=0;
+        while((r=br.read())!=-1) {
+            dates.add(br.readLine());
+        }
+        return dates;
+
+
+    }
+
 }
