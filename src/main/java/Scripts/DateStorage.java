1e68573591ba7cd781294ecf360ef02f52393a4c
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-04-29 22:39:14 -0400

    added new object variables

--- src/main/java/Scripts/DateStorage.java
+++ src/main/java/Scripts/DateStorage.java
@@ -1,16 +1,19 @@
 package Scripts;
 
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+
+
 
 public class DateStorage {
-
     ArrayList<Event> events;
 
     public DateStorage(ArrayList<Event> _events){
         events = _events;
     }
 
-    public void saveDateStorage(){
+    public void saveToFile(){
 
     }
 }
