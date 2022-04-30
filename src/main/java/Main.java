3d899d1355a082a4f62f03cfdd16b4a175d2fb32
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-04-29 23:00:18 -0400

    Updated functions for loading data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,10 @@ public class Main {
 
     public static void main(String[] agrs)  {
     }
-    LinkedList<String> dates;
-    String path = "../Databases/mainsave.txt";
-    public LinkedList<String> loadDates() throws IOException {
+
+    public LinkedList<String> loadSingleDays() throws IOException {
+        LinkedList<String> dates = null;
+        String path = "../Databases/mainsave.txt";
         File file = new File(path);
         BufferedReader br = new BufferedReader(new FileReader(file));
 
@@ -17,8 +18,19 @@ public class Main {
             dates.add(br.readLine());
         }
         return dates;
+    }
 
+    public LinkedList<String> loadRepeatedDays() throws IOException {
+        LinkedList<String> dates = null;
+        String path = "../Databases/repeat.txt";
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(file));
 
+        int r=0;
+        while((r=br.read())!=-1) {
+            dates.add(br.readLine());
+        }
+        return dates;
     }
 
 }
